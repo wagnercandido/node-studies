b1a275870261dda60df0943ca495f38894ab9f8b
wagner.canddo <wagner.candido@cwi.com.br>
2022-07-27 12:31:54 -0300

    Added use of express.json to json requests

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,28 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-	return res.json({ message: 'Hello world' });
+	const message = `Route get${req.query.name ? ' with params => ' + req.query.name : '' }`;
+	return res.json({ message });
+});
+app.post('/', (req, res) => {
+	const { name } = req.body;
+	return res.json({ message: 'Route post => ' + name });
+});
+app.get('/:id', (req, res) => {
+	const { id } = req.params;
+	return res.json({ message: 'Route get by id => ' + id });
+});
+app.put('/:id', (req, res) => {
+	const { id } = req.params;
+	const { name } = req.body;
+	return res.json({ message: `Route put by id => ${id} and name => ${name}` });
+});
+app.delete('/:id', (req, res) => {
+	const { id } = req.params;
+	return res.json({ message: `Route delete by id => ${id}` });
 });
 
 app.listen(3333);
